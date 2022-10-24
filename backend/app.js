9ab459bfb792b8f8f72b24d8b1ceb8e74be427d3
fire-walker <charlotte.churcheward@gmail.com>
2022-10-24 14:24:16 +0200

    Ajout de path, ajout de headers pour permettre les requêtes cross-origin, ajout des routeurs, ajout du gestionnaire de routage pour renvoyer des fichiers statiques pour une route donnée avec  express.static() et path.join().

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,29 @@
 const express = require('express');
-
-const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 
-mongoose.connect('mongodb+srv://cha_church:<PASSWORD>@cluster0.yywkq0y.mongodb.net/?retryWrites=true&w=majority',
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://cha_church:jubzLtCUE972MgC3@cluster0.yywkq0y.mongodb.net/?retryWrites=true&w=majority',
     { useNewUrlParser: true,
         useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-});
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
