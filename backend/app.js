e0020dc22d25bfdb9f30f67394f80772b82aa04a
fire-walker <charlotte.churcheward@gmail.com>
2022-10-25 17:19:23 +0200

    Ajout de dotenv et modif de la clé MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+require('dotenv').config();
 
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
-mongoose.connect('mongodb+srv://cha_church:jubzLtCUE972MgC3@cluster0.yywkq0y.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect(process.env.MONGODB_PATH,
     { useNewUrlParser: true,
         useUnifiedTopology: true })
     .then(() => console.log('Connexion à MongoDB réussie !'))
