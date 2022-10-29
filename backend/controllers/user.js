5351232440ab4b868c38f86cec96e5a36e371bd9
fire-walker <charlotte.churcheward@gmail.com>
2022-10-29 16:28:36 +0200

    Ajout de commentaires et variables d'environnement

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,13 +1,29 @@
+// Importation de bcrypt pour hasher le password
 const bcrypt = require('bcrypt');
+
+//Importation de JSONWebToken
 const jwt = require('jsonwebtoken');
 
+//Importation de cryptojs pour hasher l'adresse email
+const cryptojs = require('crypto-js');
+
+//Importation pour utilisation des variables d'environnement
+require('dotenv').config();
+
+//Importation models de la base de données user.js
 const User = require('../models/user');
 
+//Signup pour enregistrer le nouvel utilisateur dans la base de données
+//Chiffrage de l'email avant de l'envoyer en base de données
+//Hash du password avant de l'envoyer dans la base de données
+//Salt = 10 combien de fois sera exécuté l'algorythme de hashage
 exports.signup = (req, res, next) => {
+    const cryptoJsEmail = cryptojs.HmacSHA256(req.body.email, process.env.CRYPTOJS_SECRET_KEY).toString();
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
+            //Ce qui va être enregistré dans MongoDB
             const user = new User({
-                email: req.body.email,
+                email: cryptoJsEmail,
                 password: hash
             });
             user.save()
@@ -17,8 +33,11 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(500).json ({ error }));
 };
 
+
+//Login pour récupérer l'utilisateur déjà enregistré dans la base de données
 exports.login = (req, res, next) => {
-    User.findOne({ email: req.body.email })
+    const cryptoJsEmail = cryptojs.HmacSHA256(req.body.email, process.env.CRYPTOJS_SECRET_KEY).toString();
+    User.findOne({ email: cryptoJsEmail })
         .then(user => {
             if (!user){
                 res.status(401).json ({ message: 'Paire utilisateur/mot de passe incorrecte'});
@@ -32,7 +51,7 @@ exports.login = (req, res, next) => {
                                 userId: user._id,
                                 token: jwt.sign(
                                     { userId: user._id },
-                                    'RANDOM_TOKEN_SECRET',
+                                    process.env.RANDOM_SECRET_TOKEN,
                                     { expiresIn: '24h'}
                                 )
                             });
