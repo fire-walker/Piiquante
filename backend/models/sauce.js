2c0fab0f7c9ce78103165dd7e78bd0b662745ec5
fire-walker <charlotte.churcheward@gmail.com>
2022-11-07 14:07:52 +0100

    Modif schema + ajout de commentaires

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 
+// Utilisation de la méthode Schema de Mongoose qui contient les champs souhaités ainsi que leur type
 const sauceSchema = mongoose.Schema({
     userId: { type: String, required: true },
     name: { type: String, required: true },
-    manufacturer: { type: String, required: true},
-    description: { type: String, required: true},
-    imageUrl: { type: String, required: true},
-    heat: { type: Number, required: true},
-    likes: { type: Number, required: true},
-    dislikes: { type: Number, required: true},
-    usersLiked: { type: [String], required: true},
-    usersDisliked: { type: [String], required: true}
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number, default: 0, required: true },
+    dislikes: { type: Number, default: 0, required: true },
+    usersLiked: { type: [String], required: true },
+    usersDisliked: { type: [String], required: true }
 });
 
+// Exportation du Schema pour le rendre disponible pour notre application Express
 module.exports = mongoose.model('sauce', sauceSchema);
